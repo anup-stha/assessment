80fc228c434b1202273784af7226186a9886b494
anup-stha <anup.stha012@gmail.com>
2022-04-02 16:40:35 +0545

    feat: initial setup

--- next.config.js
+++ next.config.js
@@ -1,34 +1,10 @@
+const path = require("path");
 /** @type {import('next').NextConfig} */
-module.exports = {
-  eslint: {
-    dirs: ['src'],
-  },
-
+const nextConfig = {
   reactStrictMode: true,
-
-  // Uncoment to add domain whitelist
-  // images: {
-  //   domains: [
-  //     'res.cloudinary.com',
-  //   ],
-  // },
-
-  // SVGR
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            typescript: true,
-            icon: true,
-          },
-        },
-      ],
-    });
-
-    return config;
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
   },
 };
+
+module.exports = nextConfig;
